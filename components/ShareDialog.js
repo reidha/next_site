1cff6537632e3c927833bceded9b7da4da7a926d
reidha <kdaimon.8@gmail.com>
2020-03-31 01:31:50 +0700

    create share function

--- components/ShareDialog.js
+++ components/ShareDialog.js
@@ -2,8 +2,24 @@ import { title, url } from '../utils/constant';
 
 import { Button, Dialog, DialogTitle, DialogContent, Divider, DialogActions } from '@material-ui/core';
 import { FacebookShareButton, FacebookIcon, TwitterShareButton, TwitterIcon } from 'react-share';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
+const useStyles = makeStyles(theme => 
+    createStyles({
+        shareIconArea: {
+            padding: 10,
+            display: 'flex',
+            justifyContent: 'space-around',
+        }
+    })
+);
 
 export default function ShareDialog(props) {
+    console.log('ShareDialog');
+    const classes = useStyles(props);
+    console.log(classes);
     return (
         <>
             <Dialog
@@ -11,13 +27,19 @@ export default function ShareDialog(props) {
                 onClose={props.onClose}>
                 <DialogTitle>Share this site</DialogTitle>
                 <Divider />
-                <DialogContent>
+                <DialogContent className={classes.shareIconArea}>
                     <FacebookShareButton url={url} quote={title}>
-                        <FacebookIcon size="32" round />
+                        <FacebookIcon size="40" round />
                     </FacebookShareButton>
                     <TwitterShareButton url={url} quote={title}>
-                        <TwitterIcon size="32" round />
+                        <TwitterIcon size="40" round />
                     </TwitterShareButton>
+                </DialogContent>
+                <Divider />
+                <DialogContent className={classes.shareIconArea}>
+                    <CopyToClipboard text={url}>
+                        <Button color="primary">Copy URL</Button>
+                    </CopyToClipboard>
                 </DialogContent>
                 <Divider />
                 <DialogActions>
