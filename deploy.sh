887b9a5e387f46c56f943feaec6ba15d39e02080
reidha <kdaimon.8@gmail.com>
2020-04-05 17:28:25 +0700

    Add public/.gitignore

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@ npm run export
 cd reidha.github.io
 git init
 git remote add origin git@github.com:reidha/reidha.github.io.git
-git pull origin master
 git config user.name "reidha"
 git config user.email "kdaimon.8@gmail.com"
 git add -A
