6950f6a6afccedea8554a16591b1279a7108d98a
reidha <kdaimon.8@gmail.com>
2020-04-05 17:08:49 +0700

    Use public/

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 npm run build
 npm run export
-cp -a github/. reidha.github.io
 cd reidha.github.io
 git init
 git remote add origin git@github.com:reidha/reidha.github.io.git
+git pull origin master
 git config user.name "reidha"
 git config user.email "kdaimon.8@gmail.com"
 git add -A
