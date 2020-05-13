ab69ba6a378a215286781950eda1d21da4a62118
reidha <kdaimon.8@gmail.com>
2020-05-13 23:37:37 +0700

    Tackle reload issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,31 @@
 import PropTypes from 'prop-types';
 
+import Head from 'next/head';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import { CssBaseline } from '@material-ui/core';
 import { theme } from '../utils/theme';
+import { title } from '../utils/constant';
 
 export default function App({ Component, pageProps }) {
+    React.useEffect(() => {
+        // Remove the server-side injected CSS.
+        // eslint-disable-next-line no-undef
+        const jssStyles = document.querySelector('#jss-server-side');
+        if (jssStyles) {
+            jssStyles.parentElement.removeChild(jssStyles);
+        }
+    }, []);
+
     return (
-        <MuiThemeProvider theme={theme}>
-            <CssBaseline />
-            <Component {...pageProps} />
-        </MuiThemeProvider>
+        <>
+            <Head>
+                <title key="title">{title}</title>
+            </Head>
+            <MuiThemeProvider theme={theme}>
+                <CssBaseline />
+                <Component {...pageProps} />
+            </MuiThemeProvider>
+        </>
     )
 }
 
